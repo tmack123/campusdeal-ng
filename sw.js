ea457705e049abd1b21848e3e2cde117608a6dd3
Mordecai A. <tmack3031@gmail.com>
2026-09-12 08:47:22 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,10 @@
-// Service Worker v999 - SELF DESTRUCT & CLEAR ALL
-// This version unregisters itself and clears all caches
-
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(e) {
+// SW DISABLED
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.map(function(key) {
-          console.log('Deleting cache:', key);
-          return caches.delete(key);
-        })
-      );
-    }).then(function() {
-      // Unregister this service worker
-      return self.registration.unregister();
-    }).then(function() {
-      // Force all clients to reload
-      return self.clients.matchAll();
-    }).then(function(clients) {
-      clients.forEach(function(client) {
-        client.navigate(client.url);
-      });
-    })
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
   );
 });
-
-// Don't cache anything - pass all requests through
-self.addEventListener('fetch', function(e) {
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
